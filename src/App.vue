bcf7d6ac50d0c486b09011bef32576b9b27a03ba
Thauã Borges <thaua97@gmail.com>
2019-03-29 16:51:04 -0300

    removendo vue router e vuex

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,12 @@
 <template>
-    <router-view />
+    <Home />
 </template>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Home from '@/views/Home'
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
 }
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
